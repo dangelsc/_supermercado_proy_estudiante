6bd224f2a0757756489df4e47e7bcf765f61a72a
dangel <dange_sc@hotmail.com>
2021-02-19 16:12:45 -0400

    layout incorporado

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var ventaRouter = require('./routes/venta');
 
 var app = express();
 
+app.engine('ejs', require('ejs-locals'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
