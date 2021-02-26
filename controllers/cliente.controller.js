7714c08e58e5b85cb8796e3a94ac39952e9ceac9
dangel <dange_sc@hotmail.com>
2021-02-26 16:45:10 -0400

    generar pdf de cliente

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -1,6 +1,6 @@
 var Cliente = require("../models/cliente.model");
 //var Producto = require("../models/producto.model");
-var mongoose = require("mongoose");
+var Informes= require('./informes/informe1');
 module.exports={
     buscar:async function(req,res,next){
         let q={estado:1};
@@ -8,18 +8,25 @@ module.exports={
             q={
                 estado:1,
                 $or:[
-                {cliente:{$regex:req.body.buscar}},
-                {nit:{$regex:req.body.buscar}},
+                    {cliente:{$regex:req.body.buscar}},
+                    {nit:{$regex:req.body.buscar}},
                 ]
             }
         }
         let listacliente= await Cliente.find(q);
         console.log(listacliente);
-        res.render('cliente/buscar',
+        if(req.body.pdf){
+            let doc="Informer1_.pdf"
+            Informes.informe1(listacliente,req.body.buscar,doc,res);
+            
+        }else
+            res.render('cliente/buscar',
             {
                 msg:req.query.msg===undefined?null:req.query.msg,
                 listacliente:listacliente,
+                buscar:req.body.buscar?req.body.buscar:'',
                 error:''
             });
-    }
+        
+    }   
 }
--- routes/cliente.js
+++ routes/cliente.js
@@ -4,4 +4,5 @@ var control=  require("../controllers/cliente.controller");
 
 router.get('/buscar',control.buscar);
 router.post('/buscar',control.buscar);
+
 module.exports = router;
